ad668f515f25f20926d1e43c0cbee86149fd5482
zhaojunzhe <1429595365@qq.com>
2019-08-02 14:16:39 +0800

    react-ts-datepicker
    feat:增加面板弹出收起特性

--- src/datepicker/index.tsx
+++ src/datepicker/index.tsx
@@ -9,6 +9,7 @@ interface DatePickerProps {}
 interface DatePickerState {
   currentDate: Array<number>;
   userSelectDate: Array<number>;
+  panelVisible: boolean;
 }
 
 class DatePicker extends React.Component<DatePickerProps, DatePickerState> {
@@ -25,14 +26,19 @@ class DatePicker extends React.Component<DatePickerProps, DatePickerState> {
         newDate.getFullYear(),
         newDate.getMonth(),
         newDate.getDate()
-      ]
+      ],
+      panelVisible: false
     };
   }
-  showPanel = () => {
-    console.log(123);
+  togglePanel = () => {
+    const { panelVisible } = this.state;
+    this.setState({ panelVisible: !panelVisible });
   };
-  onChange = (date: Array<number>): void => {
+  onChange = (date: Array<number>,type: string): void => {
     this.setState({ userSelectDate: date });
+    if (type === "body") {
+      this.togglePanel();
+    }
     console.log(date);
   };
   resetDate = (): void => {
@@ -40,26 +46,30 @@ class DatePicker extends React.Component<DatePickerProps, DatePickerState> {
   };
   render() {
     const {
+      panelVisible,
       userSelectDate: [userSelectYear, userSelectMonth, userSelectDay]
     } = this.state;
     const value: string = `${userSelectYear}-${userSelectMonth +
       1}-${userSelectDay}`;
+      const panelCls = panelVisible
+      ? "datepicker-panel"
+      : "datepicker-panel hidden";
     return (
       <div className="datepicker">
         <input
           className="datepicker-input"
           readOnly
-          onClick={this.showPanel}
+          onClick={this.togglePanel}
           value={value}
         />
-        <div className="datepicker-panel">
+        <div className={panelCls}>
           <DatePickerHeader
-            handleDatePickerChange={this.onChange}
+            handleDatePickerChange={date => this.onChange(date, "header")}
             currentDate={this.state.userSelectDate}
           />
           <DatePickerBody
             currentDate={this.state.userSelectDate}
-            handleDatePickerChange={this.onChange}
+            handleDatePickerChange={date => this.onChange(date, "body")}
           />
           <DatePickerFooter resetDate={this.resetDate} />
         </div>
